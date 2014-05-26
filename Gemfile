b8e4485a4f3c1798bbb3c67338995c2e57e6db8d
Frankz Kastner <frvnkzzz@gmail.com>
2014-05-25 23:44:14 -0500

    Implemented Devise for users

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.1.0.rc2'
 
 group :development, :test do 
 	gem 'sqlite3'	
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    #Required for Heroku
+  config.action_mailer.default_url_options = { :host => 'http://arcane-shore-2097.herokuapp.com/'}
 end
