1a5e7e4b4cc698ac4fe68521cae844a3d8ee2a8b
Frankz Kastner <frvnkzzz@gmail.com>
2014-05-27 18:18:06 -0500

    Add pins/users associations

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,39 +1,37 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
   end
 
-
   def show
   end
 
-
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-      if @pin.save
-        redirect_to @pin, notice: 'Pin was successfully created.'
-      else
-        render action: 'new'
-      end
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
-
   def update
-      if @pin.update(pin_params)
-        redirect_to @pin, notice: 'Pin was successfully updated.' 
-      else
-        render action: 'edit'
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
@@ -45,6 +43,11 @@ class PinsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  def show
-  	@user = User.find(params[:id])
-  	@pins = @user.pins.page(params[:page]).per_page(20)
-  end
-end
