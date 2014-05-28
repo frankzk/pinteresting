589874c09df408249b9608a2ab1424b8f80536ae
Frankz Kastner <frvnkzzz@gmail.com>
2014-05-27 19:31:41 -0500

    Add image upload with paperclip

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.1.0.rc2'
+gem 'paperclip', '~>3.0'
 
 group :development, :test do 
 	gem 'sqlite3'	
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20140527032853) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
